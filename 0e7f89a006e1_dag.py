c16b40e217d7fab1f482636095e961980e5d6b23
asifshaj98 <asif1232@hotmail.co.uk>
2023-12-18 14:37:13 +0000

    DAG file updated

--- 0e7f89a006e1_dag.py
+++ 0e7f89a006e1_dag.py
@@ -1,20 +1,18 @@
 from airflow import DAG
-from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
-from datetime import datetime, timedelta 
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator
+from datetime import datetime, timedelta
 
-
-#Define params for Submit Run Operator
+# Define parameters for the Submit Run Operator
 notebook_task = {
     'notebook_path': '/Users/asif1232@hotmail.co.uk/Batch Processing S3 Data',
 }
 
-
-#Define params for Run Now Operator
+# Define parameters for the Run Now Operator
 notebook_params = {
-    "Variable":5
+    "Variable": 5
 }
 
-
+# Define default arguments for the DAG
 default_args = {
     'owner': 'sai charan',
     'depends_on_past': False,
@@ -24,22 +22,19 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 
-
+# Define the DAG
 with DAG('12c8b0153527_dag',
-    # should be a datetime format
-    start_date=datetime(2023, 8, 20),
-    # check out possible intervals, should be a string
-    schedule_interval='@daily',
-    catchup=False,
-    default_args=default_args
-    ) as dag:
+         start_date=datetime(2023, 8, 20),  # Start date for the DAG execution
+         schedule_interval='@daily',  # DAG schedule interval
+         catchup=False,  # Whether to catch up on missed DAG runs
+         default_args=default_args  # Default arguments for the DAG
+         ) as dag:
 
-
+    # Databricks Submit Run Operator
     opr_submit_run = DatabricksSubmitRunOperator(
         task_id='submit_run',
-        # the connection we set-up previously
-        databricks_conn_id='databricks_default',
-        existing_cluster_id='1108-162752-8okw8dgg',
-        notebook_task=notebook_task
+        databricks_conn_id='databricks_default',  # Databricks connection ID
+        existing_cluster_id='1108-162752-8okw8dgg',  # ID of the existing Databricks cluster
+        notebook_task=notebook_task  # Parameters for the notebook task
     )
-    opr_submit_run
+    opr_submit_run  # Adding the operator to the DAG
